b74317cd305899a07c00f1841f14a380092f0a5b
Helmut Kemper <helmut.kemper@gmail.com>
2019-11-16 10:46:13 -0300

    trabalhando

---
+++ typeCanvas.LineStyleFunctions.go
@@ -0,0 +1,10 @@
+package iotmaker_platform_webbrowser
+
+//lineCap
+// Sets or returns the style of the end caps for a line
+//lineJoin
+// Sets or returns the type of corner created, when two lines meet
+//lineWidth
+// Sets or returns the current line width
+//miterLimit
+// Sets or returns the maximum miter length

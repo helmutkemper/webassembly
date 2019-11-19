697b4bc342023c877dfb87fda2f0c7bfaa04ee31
Helmut Kemper <helmut.kemper@gmail.com>
2019-11-18 21:01:03 -0300

    trabalhando

--- typeCanvas.CompositingFunctions.go
+++ typeCanvas.CompositingFunctions.go
@@ -24,5 +24,28 @@ func (el *Canvas) GlobalAlpha(value float64) {
 	el.selfContext.Set("globalAlpha", value)
 }
 
-//globalCompositeOperation
-// Sets or returns how a new image are drawn onto an existing image
+// en: Sets or returns how a new image are drawn onto an existing image
+//
+//     The globalCompositeOperation property sets or returns how a source (new) image are drawn onto a destination
+//     (existing) image.
+//     source image = drawings you are about to place onto the canvas.
+//     destination image = drawings that are already placed onto the canvas.
+//
+//     Default value: source-over
+//     JavaScript syntax: context.globalCompositeOperation = "source-in";
+//     Example:
+//     var c = document.getElementById("myCanvas");
+//     var ctx = c.getContext("2d");
+//     ctx.fillStyle = "red";
+//     ctx.fillRect(20, 20, 75, 50);
+//     ctx.globalCompositeOperation = "source-over";
+//     ctx.fillStyle = "blue";
+//     ctx.fillRect(50, 50, 75, 50);
+//     ctx.fillStyle = "red";
+//     ctx.fillRect(150, 20, 75, 50);
+//     ctx.globalCompositeOperation = "destination-over";
+//     ctx.fillStyle = "blue";
+//     ctx.fillRect(180, 50, 75, 50);
+func (el *Canvas) GlobalCompositeOperation(value CanvasCompositeOperationsRule) {
+	el.selfContext.Set("globalCompositeOperation", value.String())
+}

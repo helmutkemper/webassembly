2ea6128b8c27ab724c6cd240a981a0a9c89246e8
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-02 12:11:49 -0300

    trabalhando

--- fillStyle.go
+++ fillStyle.go
@@ -1,13 +1,9 @@
 package iotmaker_platform_webbrowser
 
-import (
-	"syscall/js"
-)
-
 // en: Sets or returns the color, gradient, or pattern used to fill the drawing
 //     The fillStyle property sets or returns the color, gradient, or pattern used to fill the drawing.
 //     Default value:	#000000
 //     JavaScript syntax:	context.fillStyle = color|gradient|pattern;
 func (el *Canvas) FillStyle(value interface{}) {
-	el.SelfContext.Set("fillStyle", value.(js.Value))
+	el.SelfContext.Set("fillStyle", value)
 }

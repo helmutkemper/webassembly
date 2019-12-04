0ca1061907a5a8c198dc3e9893502c01eaa31ef3
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-04 11:36:17 -0300

    trabalhando

--- canvas/beginPath.go
+++ canvas/beginPath.go
@@ -3,6 +3,7 @@ package canvas
 //	en: Begins a path, or resets the current path
 //      Tip: Use moveTo(), lineTo(), quadricCurveTo(), bezierCurveTo(), arcTo(), and arc(), to create paths.
 //      Tip: Use the stroke() method to actually draw the path on the canvas.
+//
 // pt_br: Inicia ou reinicializa uma nova rota no desenho
 //      Dica: Use moveTo(), lineTo(), quadricCurveTo(), bezierCurveTo(), arcTo(), e arc(), para criar uma nova rota no desenho
 //      Dica: Use o método stroke() para desenhar a rota no elemento canvas

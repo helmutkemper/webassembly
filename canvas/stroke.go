c4036a038692ac846cd090f0c6e923c58c80b03e
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-04 11:33:12 -0300

    trabalhando

--- canvas/stroke.go
+++ canvas/stroke.go
@@ -1,9 +1,10 @@
 package canvas
 
-// en: Actually draws the path you have defined
-//     The stroke() method actually draws the path you have defined with all those moveTo() and lineTo() methods. The
-//     default color is black.
+// en: The stroke() method actually draws the path you have defined with all those moveTo() and lineTo() methods. The default color is black.
 //     Tip: Use the strokeStyle property to draw with another color/gradient.
+//
+// pt_br: O método stroke() desenha o caminho definido com os métodos moveTo() e lineTo() usando a cor padrão, preta.
+//     Dica: Use a propriedade strokeStyle para desenhar com outra cor ou usar um gradiente
 func (el *Canvas) Stroke() {
 	el.SelfContext.Call("stroke")
 }

74c48ce4dea2cab575ae2ee16f073dab32b4ce1d
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-04 23:22:59 -0300

    trabalhando

---
+++ doc.go
@@ -0,0 +1,2 @@
+// en: This package increment web browser specific functions
+package iotmaker_platform_webbrowser
---
+++ mouse/doc.go
@@ -0,0 +1,4 @@
+// en: This package increment mouse functions
+//
+// reference: https://www.w3schools.com/jsref/prop_style_cursor.asp
+package mouse

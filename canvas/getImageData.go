e020a802c5cf2beea50c89c71a1943fe8c960b47
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-07 11:00:18 -0300

    trabalhando

--- canvas/getImageData.go
+++ canvas/getImageData.go
@@ -4,55 +4,54 @@ import (
 	"image/color"
 )
 
-// en: Returns an ImageData object that copies the pixel data for the specified rectangle on a canvas
-//     x:      The x coordinate (in pixels) of the upper-left corner to start copy from
-//     y:      The y coordinate (in pixels) of the upper-left corner to start copy from
-//     width:  The width of the rectangular area you will copy
+// en: Returns an ImageData map[x][y]color.RGBA that copies the pixel data for the
+// specified rectangle on a canvas
+//     x: The x coordinate (in pixels) of the upper-left corner to start copy from
+//     y: The y coordinate (in pixels) of the upper-left corner to start copy from
+//     width: The width of the rectangular area you will copy
 //     height: The height of the rectangular area you will copy
 //     return: map[x(int)][y(int)]color.RGBA
-//             Note: return x and y are NOT relative to the coordinate (0,0) on the image, are relative to the
-//                   coordinate (0,0) on the canvas
+//             Note: return x and y are NOT relative to the coordinate (0,0) on the
+//             image, are relative to the coordinate (0,0) on the canvas
 //
-//     JavaScript syntax: context.getImageData(x, y, width, height);
+//     Note: The ImageData object is not a picture, it specifies a part (rectangle)
+//     on the canvas, and holds information of every pixel inside that rectangle.
 //
-//     The getImageData() method returns an ImageData object that copies the pixel data for the specified rectangle on a
-//     canvas.
-//     Note: The ImageData object is not a picture, it specifies a part (rectangle) on the canvas, and holds information
-//     of every pixel inside that rectangle.
-//
-//     For every pixel in an ImageData object there are four pieces of information, the RGBA values:
+//     For every pixel in the map[x][y] there are four pieces of information, the
+//     color.RGBA values:
 //     R - The color red (from 0-255)
 //     G - The color green (from 0-255)
 //     B - The color blue (from 0-255)
 //     A - The alpha channel (from 0-255; 0 is transparent and 255 is fully visible)
 //
-//     The color/alpha information is held in an array, and is stored in the data property of the ImageData object.
-//     Tip: After you have manipulated the color/alpha information in the array, you can copy the image data back onto
-//     the canvas with the putImageData() method.
+//     Tip: After you have manipulated the color/alpha information in the map[x][y],
+//     you can copy the image data back onto the canvas with the putImageData()
+//     method.
 //
-//     Example:
-//     The code for getting color/alpha information of the first pixel in the returned ImageData object:
-//          red = imgData.data[0];
-//          green = imgData.data[1];
-//          blue = imgData.data[2];
-//          alpha = imgData.data[3];
+// pr_br: Retorna um mapa map[x][y]color.RGBA com parte dos dados da imagem contida
+// no retângulo especificado.
+//     x: Coordenada x (em pixels) do canto superior esquerdo de onde os dados vão
+//     ser copiados
+//     y: Coordenada y (em pixels) do canto superior esquerdo de onde os dados vão
+//     ser copiados
+//     width: comprimento do retângulo a ser copiado
+//     height: altura do retângulo a ser copiado
+//     return: map[x(int)][y(int)]color.RGBA
+//             Nota: x e y do retorno não são relativos a coordenada (0,0) da imagem,
+//             são relativos a coordenada (0,0) do canvas
 //
-//     Tip: You can also use the getImageData() method to invert the color of every pixels of an image on the canvas.
+//     Nota: Os dados da imagem não são uma figura, eles representam uma parte
+//     retangular do canvas e guardam informações de cada pixel contido nessa área
 //
-//     Loop through all the pixels and change the color values using this formula:
-//          red = 255-old_red;
-//          green = 255-old_green;
-//          blue = 255-old_blue;
+//     Para cada pixel contido no mapa há quatro peças de informação com valores no
+//     formato de color.RGBA:
+//     R - Cor vermelha (de 0-255)
+//     G - Cor verde (de 0-255)
+//     B - Cor azul (de 0-255)
+//     A - Canal alpha (de 0-255; onde, 0 é transparente e 255 é totalmente visível)
 //
-//     Example:
-//     var c = document.getElementById("myCanvas");
-//     var ctx = c.getContext("2d");
-//     ctx.fillStyle = "red";
-//     ctx.fillRect(10, 10, 50, 50);
-//     function copy() {
-//       var imgData = ctx.getImageData(10, 10, 50, 50);
-//       ctx.putImageData(imgData, 10, 70);
-//     }
+//     Dica: Depois de manipular as informações de cor/alpha contidas no map[x][y],
+//     elas podem ser colocadas de volta no canvas com o método putImageData().
 func (el *Canvas) GetImageData(x, y, width, height int) map[int]map[int]color.RGBA {
 
 	dataInterface := el.SelfContext.Call("getImageData", x, y, width, height)

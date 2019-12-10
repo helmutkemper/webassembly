87c2b059f95f363a8c993d4891ad2b7233a74c00
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-10 13:22:14 -0300

    trabalhando

---
+++ examples/htmlImage/main.go
@@ -0,0 +1,27 @@
+// +build js
+
+package main
+
+import (
+	"github.com/helmutkemper/iotmaker.platform.webbrowser/factoryBrowserDocument"
+	"github.com/helmutkemper/iotmaker.platform.webbrowser/factoryBrowserHtml"
+)
+
+func main() {
+
+	done := make(chan struct{}, 0)
+
+	browserDocument := factoryBrowserDocument.NewDocument()
+
+	factoryBrowserHtml.NewImage(
+		browserDocument.SelfDocument,
+		map[string]interface{}{
+			"id":  "player",
+			"src": "./player_big.png",
+		},
+		true,
+		true,
+	)
+
+	<-done
+}

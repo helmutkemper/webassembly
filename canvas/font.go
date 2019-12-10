a68d8304ecee0d6d981bf6b27f5584aba27693d0
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-10 16:19:27 -0300

    trabalhando

---
+++ canvas/font.go
@@ -0,0 +1,10 @@
+package canvas
+
+import "github.com/helmutkemper/iotmaker.platform.webbrowser/font"
+
+// en: Sets the current font properties for text content
+//
+// pt_br: Define as propriedades da fonte atual
+func (el *Canvas) Font(font font.Font) {
+	el.SelfContext.Set("font", font.String())
+}

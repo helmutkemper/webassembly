eb349dbfd7e6285ecbe4372e6c15b953ac251f1a
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-12 14:35:54 -0300

    trabalhando

--- canvas/resetShadow.go
+++ canvas/resetShadow.go
@@ -1,6 +1,6 @@
 package canvas
 
-func (el *Canvas) ShadowReset() {
+func (el *Canvas) ResetShadow() {
 	el.SelfContext.Set("shadowOffsetX", 0)
 	el.SelfContext.Set("shadowOffsetY", 0)
 	el.SelfContext.Set("shadowBlur", 0)

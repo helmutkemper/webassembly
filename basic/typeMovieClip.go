e18f97f0eae5d57b0b3342d063f74f9253a9f43b
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-25 15:31:16 -0300

    sprite, movieClip and timeLine

---
+++ basic/typeMovieClip.go
@@ -0,0 +1,10 @@
+package basic
+
+import (
+	iotmaker_platform_IDraw "github.com/helmutkemper/iotmaker.platform.IDraw"
+)
+
+type MovieClip struct {
+	Platform iotmaker_platform_IDraw.IDraw
+	TimeLine TimeLine
+}
---
+++ basic/typeTimeLine.go
@@ -0,0 +1,8 @@
+package basic
+
+import "time"
+
+type TimeLine struct {
+	Time   time.Duration
+	Sprite Sprite
+}

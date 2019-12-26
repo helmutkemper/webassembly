dae0ed9dcce9741cb59e02c2c0300f1acc92e4e8
helmutkemper <helmut.kemper@ahgora.com.br>
2019-12-26 16:27:01 -0300

    novo conceito

--- canvas/typeStage.go
+++ canvas/typeStage.go
@@ -1,46 +1,22 @@
 package canvas
 
-import (
-	iotmaker_platform_coordinate "github.com/helmutkemper/iotmaker.platform.coordinate"
-)
-
 // todo: density
 type Stage struct {
 	Canvas
-	ScratchPad     Canvas
-	Density        float64
-	originalX      float64
-	originalY      float64
-	originalHeight float64
-	originalWidth  float64
-	X              iotmaker_platform_coordinate.Density
-	Y              iotmaker_platform_coordinate.Density
-	Width          iotmaker_platform_coordinate.Density
-	Height         iotmaker_platform_coordinate.Density
-}
-
-func (el *Stage) SetDensityFactor(density float64) {
-	el.Density = density
-	el.Width.SetDensityFactor(density)
-	el.Height.SetDensityFactor(density)
-}
-
-func (el *Stage) SetX(x float64) {
-	el.originalX = x
-	el.X.Set(x)
-}
-
-func (el *Stage) SetY(y float64) {
-	el.originalY = y
-	el.Y.Set(y)
+	ScratchPad Canvas
+	Density    float64
+	Width      float64
+	Height     float64
 }
 
 func (el *Stage) SetWidth(width float64) {
-	el.originalWidth = width
-	el.Width.Set(width)
+	el.Width = width
 }
 
 func (el *Stage) SetHeight(height float64) {
-	el.originalHeight = height
-	el.Height.Set(height)
+	el.Height = height
+}
+
+func (el *Stage) Clear() {
+	el.ClearRect(0, 0, el.Width, el.Height)
 }
--- factoryBrowserStage/newStage.go
+++ factoryBrowserStage/newStage.go
@@ -5,6 +5,7 @@ import (
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/canvas"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/document"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/factoryBrowserCanvas"
+	"github.com/helmutkemper/iotmaker.platform/fps"
 )
 
 func NewStage(document document.Document, id string, width, height float64, density interface{}, iDensity iotmaker_platform_coordinate.IDensity) canvas.Stage {
@@ -14,15 +15,17 @@ func NewStage(document document.Document, id string, width, height float64, dens
 	densityCalc.SetDensityFactor(density)
 
 	densityCalc.Set(width)
-	width = densityCalc.Float64()
+	stage.Width = densityCalc.Float64()
 
 	densityCalc.Set(height)
-	height = densityCalc.Float64()
+	stage.Height = densityCalc.Float64()
 
-	stage.Canvas = factoryBrowserCanvas.NewCanvasWith2DContext(document.SelfDocument, id, width, height)
-	stage.ScratchPad = factoryBrowserCanvas.NewCanvasWith2DContext(document.SelfDocument, id+"ScratchPad", width, height)
+	stage.Canvas = factoryBrowserCanvas.NewCanvasWith2DContext(document.SelfDocument, id, stage.Width, stage.Height)
+	stage.ScratchPad = factoryBrowserCanvas.NewCanvasWith2DContext(document.SelfDocument, id+"ScratchPad", stage.Width, stage.Height)
 
 	document.AppendChildToDocumentBody(stage.SelfElement)
+
+	fps.AddRunner(stage.Clear, false)
 
 	return stage
 }

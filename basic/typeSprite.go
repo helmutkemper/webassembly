69034cbb04a1c26bfb5d88eddcde8f709ddd6ad9
helmutkemper <helmut.kemper@ahgora.com.br>
2019-12-27 12:02:21 -0300

    correções

--- basic/typeSprite.go
+++ basic/typeSprite.go
@@ -7,6 +7,7 @@ import (
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/font"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/mouse"
 	"github.com/helmutkemper/iotmaker.platform/abstractType/genericTypes"
+	platformMouse "github.com/helmutkemper/iotmaker.platform/mouse"
 	"image/color"
 	"time"
 )
@@ -33,6 +34,25 @@ func (el *Sprite) SetPlatform(platform iotmaker_platform_IDraw.IDraw) {
 
 func (el *Sprite) GetPlatform() iotmaker_platform_IDraw.IDraw {
 	return el.Platform
+}
+
+func (el *Sprite) SetDraggable(enable bool) {
+	el.IsDraggable = enable
+	if enable == true {
+		platformMouse.AddFunctionPointer("size", el.GetCollisionBox, func(x, y float64, collision bool) {
+			//if collision == true {
+			el.OutBoxDimensions.X = x - el.OutBoxDimensions.Width/2
+			el.Dimensions.X = x - el.Dimensions.Width/2
+			el.OutBoxDimensions.Y = y - el.OutBoxDimensions.Height/2
+			el.Dimensions.Y = y - el.Dimensions.Height/2
+			//}
+		})
+	}
+}
+
+func (el *Sprite) GetCollisionBox(xEvent, yEvent float64) bool {
+	return el.OutBoxDimensions.X <= xEvent && el.OutBoxDimensions.X+el.OutBoxDimensions.Width >= xEvent &&
+		el.OutBoxDimensions.Y <= yEvent && el.OutBoxDimensions.Y+el.OutBoxDimensions.Height >= yEvent
 }
 
 // en: Begins a path, or resets the current path

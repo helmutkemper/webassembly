d399e94a1b51f420ea6f4ddd1c3b378dc07da7fd
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-05 11:12:46 -0300

    trabalhando

--- factoryBrowserStage/newStage.go
+++ factoryBrowserStage/newStage.go
@@ -59,6 +59,9 @@ func NewStage(htmlPlatform iotmaker_platform_IDraw.IHtml, document document.Docu
 
 			stage.ScratchPad.SetWidth(stage.Width)
 			stage.ScratchPad.SetHeight(stage.Height)
+
+			stage.Cache.SetWidth(stage.Width)
+			stage.Cache.SetHeight(stage.Height)
 		}
 	})
 

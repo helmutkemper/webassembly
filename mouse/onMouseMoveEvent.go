edc32d9967b88d5343070bcf830b2b815534f8d3
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-14 19:38:37 -0300

    engine

--- mouse/onMouseMoveEvent.go
+++ mouse/onMouseMoveEvent.go
@@ -1,6 +1,7 @@
 package mouse
 
 import (
+	"fmt"
 	"syscall/js"
 )
 
@@ -8,11 +9,15 @@ var mouseMoveEvt js.Func
 var X int
 var Y int
 
+var mouseChannelCoordinate chan []int
+
 func SetMouseMoveManager() js.Func {
 	mouseMoveEvt = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		e := args[0]
 		X = e.Get("clientX").Int()
 		Y = e.Get("clientY").Int()
+
+		mouseChannelCoordinate <- []int{X, Y}
 
 		return nil
 	})
@@ -22,4 +27,17 @@ func SetMouseMoveManager() js.Func {
 
 func ReleaseMouseMoveListener() {
 	mouseMoveEvt.Release()
+}
+
+func init() {
+	mouseChannelCoordinate = make(chan []int)
+
+	go func() {
+		//var coordinate = make( chan mouseChnn.CoordinateChannel )
+		for {
+
+			fmt.Printf("(%v)\n", <-mouseChannelCoordinate)
+
+		}
+	}()
 }

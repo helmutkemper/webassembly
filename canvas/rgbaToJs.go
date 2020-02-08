b665b1f435bb30706cc809a24da015a747ee81e9
Helmut Kemper <helmut.kemper@gamil.com>
2020-02-07 21:12:28 -0300

    trabalhando

--- canvas/rgbaToJs.go
+++ canvas/rgbaToJs.go
@@ -6,5 +6,5 @@ import (
 )
 
 func RGBAToJs(color color.RGBA) string {
-	return "rgba( " + strconv.Itoa(int(color.R)) + ", " + strconv.Itoa(int(color.G)) + ", " + strconv.Itoa(int(color.B)) + ", " + strconv.Itoa(int(color.A)) + " )"
+	return "rgba( " + strconv.Itoa(int(color.R)) + ", " + strconv.Itoa(int(color.G)) + ", " + strconv.Itoa(int(color.B)) + ", " + strconv.FormatFloat(float64(color.A)/255.0, 'f', 4, 64) + " )"
 }

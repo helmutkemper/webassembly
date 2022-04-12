34d7b517a79967f544d3928aede8dbd39276a16d
Helmut Kemper <helmut.kemper@gmail.com>
2022-04-11 21:31:52 -0300

    working

--- examples/animation/main.go
+++ examples/animation/main.go
@@ -9,8 +9,7 @@ import (
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/css"
 	document2 "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/document"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowserImage"
-	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/mathUtil"
-	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/tween"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/factoryTween"
 	"log"
 	"strconv"
 	"time"
@@ -46,58 +45,33 @@ func main() {
 		}
 		var e = document.GetElementById(document, id)
 
-		a := tween.Tween{}
-		a.
-			SetDuration(
-				time.Duration(mathUtil.Int(2000, 5000))*time.Millisecond,
-			).
-			SetValues(
-				mathUtil.Float64FomInt(0, global.Global.Document.GetDocumentWidth()-29),
-				mathUtil.Float64FomInt(0, global.Global.Document.GetDocumentWidth()-29),
-			).
-			SetOnStepFunc(
-				func(x, p float64, ars ...interface{}) {
-					px := strconv.FormatFloat(x, 'E', 10, 32) + "px"
-					document.SetElementStyle(e, "left", px)
-				},
-			).
-			SetLoops(-1).
-			SetTweenFunc(tween.SelectRandom()).
-			Start()
+		factoryTween.NewEaseInBack(
+			//time.Duration(mathUtil.Int(2000, 5000))*time.Millisecond,
+			2*time.Second,
+			//mathUtil.Float64FomInt(0, global.Global.Document.GetDocumentWidth()-29),
+			//mathUtil.Float64FomInt(0, global.Global.Document.GetDocumentWidth()-29),
+			0,
+			float64(global.Global.Document.GetDocumentWidth()-29),
+			func(x, p float64, ars ...interface{}) {
+				px := strconv.FormatFloat(x, 'E', 10, 32) + "px"
+				document.SetElementStyle(e, "left", px)
+			},
+			-1,
+		)
 
-		b := tween.Tween{}
-		b.
-			SetDuration(
-				time.Duration(mathUtil.Int(2000, 5000))*time.Millisecond,
-			).
-			SetValues(
-				mathUtil.Float64FomInt(0, global.Global.Document.GetDocumentHeight()-50),
-				mathUtil.Float64FomInt(0, global.Global.Document.GetDocumentHeight()-50),
-			).
-			SetOnStepFunc(
-				func(y, p float64, ars ...interface{}) {
-					py := strconv.FormatFloat(y, 'E', 10, 32) + "px"
-					document.SetElementStyle(e, "top", py)
-				},
-			).
-			SetOnEndFunc(func(value float64, arguments ...interface{}) {
-				log.Printf("end function")
-			}).
-			SetOnStartFunc(func(value float64, arguments ...interface{}) {
-				log.Printf("start function")
-			}).
-			SetOnCycleStartFunc(func(value float64, arguments ...interface{}) {
-				log.Printf("start ciclo")
-			}).
-			SetOnCycleEndFunc(func(value float64, arguments ...interface{}) {
-				log.Printf("end ciclo")
-			}).
-			SetOnInvertFunc(func(value float64, arguments ...interface{}) {
-				log.Printf("inverte")
-			}).
-			SetLoops(-1).
-			SetTweenFunc(tween.SelectRandom()).
-			Start()
+		factoryTween.NewEaseInBack(
+			//time.Duration(mathUtil.Int(2000, 5000))*time.Millisecond,
+			2*time.Second,
+			//mathUtil.Float64FomInt(0, global.Global.Document.GetDocumentHeight()-50),
+			0,
+			//mathUtil.Float64FomInt(0, global.Global.Document.GetDocumentHeight()-50),
+			float64(global.Global.Document.GetDocumentHeight()-50),
+			func(y, p float64, ars ...interface{}) {
+				py := strconv.FormatFloat(y, 'E', 10, 32) + "px"
+				document.SetElementStyle(e, "top", py)
+			},
+			-1,
+		)
 
 	}
 

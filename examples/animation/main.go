9520aa18dcd6d434f925370e00032571d9449598
Helmut Kemper <helmut.kemper@gmail.com>
2022-04-15 23:12:40 -0300

    working

--- examples/animation/main.go
+++ examples/animation/main.go
@@ -6,8 +6,6 @@ package main
 
 import (
 	global "github.com/helmutkemper/iotmaker.santa_isabel_theater.globalConfig"
-	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/browserMouse"
-	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/css"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowser"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowserImage"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/html"
@@ -33,95 +31,51 @@ func main() {
 		false,
 	)
 
-	var class = new(css.Class)
-	// Create a css list named "red" with value "user red"
-	// Crie uma lista css de nome "red" com o valor "user red"
-	class.SetList("red", "user", "red").
-		// Create a css list named "yellow" with value "user yellow"
-		// Crie uma lista css de nome "yellow" com o valor "user yellow"
-		SetList("yellow", "user", "yellow").
-		// Create a css list named "user" with value "user"
-		// Crie uma lista css de nome "user" com o valor "user"
-		SetList("user", "user").
-		// Defines that the "red" and "yellow" lists will change every second
-		// Define que as listas "red" e "yellow" vão trocar a cada segundo
-		ToggleTime(time.Second, "red", "yellow").
-		// Limit trades to 10 interactions
-		// Limita as trocas em 10 interações
-		ToggleLoop(10).
-		// Defines the list named "norm" as the active list at the end of interactions
-		// Define  alista de nome "normal" como sendo a lista ativa ao final das interações
-		OnLoopEnd("user").
-		// Start interactions. Caution: they only work after being added to the element
-		// Inicia as interações. Cuidado: elas só funcionam após serem adicionadas ao elemento
-		ToggleStart()
+	var border = 200
 
-	factoryBrowser.NewTagButton("example_tag_A").
-		SetHRef("#").
-		SetText("Estou vivo!").
-		AppendById("stage")
+	for a := 0; a != 20; a += 1 {
 
-	// Create a div with id "example";
-	// Cria uma div de id "example_A";
-	factoryBrowser.NewTagDiv("example_A").
-		// Sets css to be "name_a name_b name_N";
-		// Define css como sendo "name_a name_b name_N";
-		SetClass("animate").
-		SetHidden().
-		SetDraggable(html.KDraggableYes).
-		SetMousePointer(browserMouse.KCursorMove).
-		SetTranslate(html.KTranslateYes).
-		SetDir(html.KDirAuto).
-		SetAccessKey("a").
-		SetLang(html.KLanguagePortuguese).
-		// Adds the div to the element id "stage".
-		// Adiciona a div ao elemento de id "stage".
-		AppendById("stage")
+		var intervalX = time.Duration(mathUtil.Int(1000, 3000)) * time.Millisecond
+		var intervalY = time.Duration(mathUtil.Int(1000, 3000)) * time.Millisecond
+		var width = global.Global.Document.GetDocumentWidth() - 29 - border
+		var height = global.Global.Document.GetDocumentHeight() - 50 - border
+		var positionInicialX = mathUtil.Float64FomInt(border, width)
+		var positionFinalX = mathUtil.Float64FomInt(border, width)
+		var positionInicialY = mathUtil.Float64FomInt(border, height)
+		var positionFinalY = mathUtil.Float64FomInt(border, height)
 
-	// Create a div with id "example";
-	// Cria uma div de id "example_A";
-	factoryBrowser.NewTagDiv("example_B").
-		// Sets css to be "name_a name_b name_N";
-		// Define css como sendo "name_a name_b name_N";
-		SetClass("name_a", "name_b", "name_N").
-		// css.Class cannot work properly before being added, due to lack of reference to the parent
-		// object.
-		// css.Class não consegue funcionar corretamente antes de ser adicionada, por falta de referência
-		// do objeto pai.
-		SetCssController(class).
-		// Adds the div to the element id "stage".
-		// Adiciona a div ao elemento de id "stage".
-		AppendById("stage")
+		var id = "div_" + strconv.FormatInt(int64(a), 10)
 
-	//document.GetElementById(document, "stage")
-	for a := 0; a != 10; a += 1 {
-
-		id := "div_" + strconv.FormatInt(int64(a), 10)
 		rocket := factoryBrowser.NewTagDiv(id).
 			SetClass("animate").
 			AppendById("stage")
 
-		var border = 300
 		factoryTween.NewSelectRandom(
-			time.Duration(mathUtil.Int(1000, 3000))*time.Millisecond,
-			mathUtil.Float64FomInt(border, global.Global.Document.GetDocumentWidth()-29-border),
-			mathUtil.Float64FomInt(border, global.Global.Document.GetDocumentWidth()-29-border),
-			func(x, p float64, ars ...interface{}) {
-				rocket.SetX(int(x))
-			},
+			intervalX,
+			positionInicialX,
+			positionFinalX,
+			updateX,
 			-1,
+			rocket,
 		)
 
 		factoryTween.NewSelectRandom(
-			time.Duration(mathUtil.Int(1000, 3000))*time.Millisecond,
-			mathUtil.Float64FomInt(border, global.Global.Document.GetDocumentHeight()-50-border),
-			mathUtil.Float64FomInt(border, global.Global.Document.GetDocumentHeight()-50-border),
-			func(y, p float64, ars ...interface{}) {
-				rocket.SetY(int(y))
-			},
+			intervalY,
+			positionInicialY,
+			positionFinalY,
+			updateY,
 			-1,
+			rocket,
 		)
 	}
 
 	<-done
+}
+
+func updateX(x, p float64, args ...interface{}) {
+	args[0].([]interface{})[0].(*html.GlobalAttributes).SetX(int(x))
+}
+
+func updateY(y, p float64, args ...interface{}) {
+	args[0].([]interface{})[0].(*html.GlobalAttributes).SetY(int(y))
 }

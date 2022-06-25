7d9fc487c06e2631ff01f65f7f388f51fedd659e
Helmut Kemper <helmut.kemper@gmail.com>
2022-06-25 15:55:14 -0300

    working

--- examples/svg/a/main.go
+++ examples/svg/a/main.go
@@ -1,7 +1,68 @@
+// English:
+//
+// This example was taken from https://developer.mozilla.org/pt-BR/docs/Web/SVG/Element/a
+//
+//   Notes:
+//     * The CSS is inside the example HTML file.
+//
+// Português:
+//
+// Este exemplo foi retirado do site https://developer.mozilla.org/pt-BR/docs/Web/SVG/Element/a
+//
+//   Notas:
+//     * O CSS está dentro do arquivo HTML de exemplo.
+//
+// CSS:
+//
+//  /* As SVG does not provide a default visual style for links,
+//     it's considered best practice to add some */
+//
+//  /* Como o SVG não fornece um estilo visual padrão para links,
+//     é considerado uma prática recomendada adicionar algum */
+//
+//  @namespace svg url(http://www.w3.org/2000/svg);
+//
+//  /* Necessary to select only SVG <a> elements, and not also HTML’s.
+//     See warning below */
+//
+//  /* Necessário selecionar apenas elementos SVG <a>, e não também HTML.
+//     Veja o aviso abaixo */
+//
+//  svg|a:link, svg|a:visited {
+//    cursor: pointer;
+//  }
+//
+//  svg|a text,
+//  text svg|a {
+//    fill: blue; /* Even for text, SVG uses fill over color */
+//                /* Mesmo para texto, o SVG usa preenchimento sobre cor */
+//    text-decoration: underline;
+//  }
+//
+//  svg|a:hover, svg|a:active {
+//    outline: dotted 1px blue;
+//  }
+//
+// HTML:
+//
+//  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+//    <!-- A link around a shape -->
+//    <a href="/docs/Web/SVG/Element/circle">
+//      <circle cx="50" cy="40" r="35"/>
+//    </a>
+//
+//    <!-- A link around a text -->
+//    <a href="/docs/Web/SVG/Element/text">
+//      <text x="50" y="90" text-anchor="middle">
+//        &lt;circle&gt;
+//      </text>
+//    </a>
+//  </svg>
+//
+
 //go:build js
 // +build js
 
-//
 package main
 
 import (
@@ -13,48 +74,7 @@ func main() {
 
 	done := make(chan struct{}, 0)
 
-	// browser stage
 	stage := factoryBrowser.NewStage()
-
-	// https://developer.mozilla.org/pt-BR/docs/Web/SVG/Element/a
-	//
-	//  /* As SVG does not provide a default visual style for links,
-	//     it's considered best practice to add some */
-	//
-	//  @namespace svg url(http://www.w3.org/2000/svg);
-	//  /* Necessary to select only SVG <a> elements, and not also HTML’s.
-	//     See warning below */
-	//
-	//  svg|a:link, svg|a:visited {
-	//    cursor: pointer;
-	//  }
-	//
-	//  svg|a text,
-	//  text svg|a {
-	//    fill: blue; /* Even for text, SVG uses fill over color */
-	//    text-decoration: underline;
-	//  }
-	//
-	//  svg|a:hover, svg|a:active {
-	//    outline: dotted 1px blue;
-	//  }
-	//
-	//  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-	//    <!-- A link around a shape -->
-	//    <a href="/docs/Web/SVG/Element/circle">
-	//      <circle cx="50" cy="40" r="35"/>
-	//    </a>
-	//
-	//    <!-- A link around a text -->
-	//    <a href="/docs/Web/SVG/Element/text">
-	//      <text x="50" y="90" text-anchor="middle">
-	//        &lt;circle&gt;
-	//      </text>
-	//    </a>
-	//  </svg>
-	//
-	//  Notes:
-	//    * The CSS is inside the example HTML file.
 
 	s1 := factoryBrowser.NewTagSvg("svg1").
 		ViewBox([]float64{0, 0, 100, 100}).
--- examples/svg/svg/main.go
+++ examples/svg/svg/main.go
@@ -1,7 +1,23 @@
+// English:
+//
+// This example was taken from https://developer.mozilla.org/pt-BR/docs/Web/SVG/Element/svg
+//
+// Português:
+//
+// Este exemplo foi retirado do site https://developer.mozilla.org/pt-BR/docs/Web/SVG/Element/svg
+//
+//<svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg" stroke="red" fill="grey">
+//  <circle cx="50" cy="50" r="40" />
+//  <circle cx="150" cy="50" r="4" />
+//
+//  <svg viewBox="0 0 10 10" x="200" width="100">
+//    <circle cx="5" cy="5" r="4" />
+//  </svg>
+//</svg>
+
 //go:build js
 // +build js
 
-//
 package main
 
 import (
@@ -13,47 +29,30 @@ func main() {
 
 	done := make(chan struct{}, 0)
 
-	// browser stage
 	stage := factoryBrowser.NewStage()
 
-	// https://developer.mozilla.org/pt-BR/docs/Web/SVG/Element/svg
-	//
-	//<svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg" stroke="red" fill="grey">
-	//  <circle cx="50" cy="50" r="40" />
-	//  <circle cx="150" cy="50" r="4" />
-	//
-	//  <svg viewBox="0 0 10 10" x="200" width="100">
-	//    <circle cx="5" cy="5" r="4" />
-	//  </svg>
-	//</svg>
-
-	//<svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg" stroke="red" fill="grey">
 	s1 := factoryBrowser.NewTagSvg("svg1").
 		ViewBox([]float64{0, 0, 300, 100}).
 		Stroke(factoryColor.NewRed()).
 		Fill(factoryColor.NewGrey()).
 		Append(
 
-			//  <circle cx="50" cy="50" r="40" />
 			factoryBrowser.NewTagSvgCircle("cir1").
 				Cx(50).
 				Cy(50).
 				R(40),
 
-			//  <circle cx="150" cy="50" r="4" />
 			factoryBrowser.NewTagSvgCircle("cir2").
 				Cx(150).
 				Cy(50).
 				R(4),
 
-			//  <svg viewBox="0 0 10 10" x="200" width="100">
 			factoryBrowser.NewTagSvg("svg2").
 				ViewBox([]float64{0, 0, 10, 10}).
 				X(200).
 				Width(100).
 				Append(
 
-					//    <circle cx="5" cy="5" r="4" />
 					factoryBrowser.NewTagSvgCircle("cir3").
 						Cx(5).
 						Cy(5).

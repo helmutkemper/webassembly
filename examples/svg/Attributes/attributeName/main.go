d5547f8e495c3a5e307316e61a4e9ae4a52d4f85
Helmut Kemper <helmut.kemper@gmail.com>
2022-07-03 16:39:25 -0300

    working

---
+++ examples/svg/Attributes/attributeName/main.go
@@ -0,0 +1,62 @@
+// English:
+//
+// This example was taken from https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/attributeName
+//
+// Português:
+//
+// Este exemplo foi retirado do site https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/attributeName
+//
+//  <svg viewBox="0 0 250 250" xmlns="http://www.w3.org/2000/svg">
+//    <rect x="50" y="50" width="100" height="100">
+//      <animate attributeType="XML" attributeName="y" from="0" to="50"
+//          dur="5s" repeatCount="indefinite"/>
+//    </rect>
+//  </svg>
+
+//go:build js
+// +build js
+
+package main
+
+import (
+	"github.com/helmutkemper/iotmaker.webassembly/browser/factoryBrowser"
+	"github.com/helmutkemper/iotmaker.webassembly/browser/html"
+	"time"
+)
+
+func main() {
+
+	done := make(chan struct{}, 0)
+
+	stage := factoryBrowser.NewStage()
+
+	s1 := factoryBrowser.NewTagSvg().
+		ViewBox([]float64{0, 0, 250, 250}).
+		Append(
+
+			factoryBrowser.NewTagSvgRect().X(50).Y(50).Width(100).Height(100).Append(
+
+				factoryBrowser.NewTagSvgAnimate().AttributeName("y").From(0).To(50).Dur(5 * time.Second).RepeatCount(html.KSvgDurIndefinite),
+			),
+		)
+
+	stage.Append(s1)
+
+	<-done
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
---
+++ examples/svg/Attributes/begin/AccesskeyExample/main.go
@@ -0,0 +1,110 @@
+// English:
+//
+// This example was taken from https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/begin
+//
+// Português:
+//
+// Este exemplo foi retirado do site https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/begin
+//
+//  <svg width="120" height="120"  viewBox="0 0 120 120"
+//       xmlns="http://www.w3.org/2000/svg" version="1.1"
+//       xmlns:xlink="http://www.w3.org/1999/xlink">
+//
+//      <!-- animated rectangles -->
+//      <rect x="10" y="35" height="15" width="0">
+//          <animate attributeType="XML" attributeName="width" from="0" to="100"
+//                   begin="accessKey(s)" dur="8s"
+//                   fill="freeze" />
+//      </rect>
+//
+//      <!-- trigger -->
+//      <text x="60" y="80" text-anchor="middle"
+//            style="pointer-events:none;">Hit the "s" key</text>
+//
+//      <!-- grid -->
+//      <text x="10" y="20" text-anchor="middle">0s</text>
+//      <line x1="10" y1="25" x2="10" y2="55" stroke="grey" stroke-width=".5" />
+//      <text x="35" y="20" text-anchor="middle">2s</text>
+//      <line x1="35" y1="25" x2="35" y2="55" stroke="grey" stroke-width=".5" />
+//      <text x="60" y="20" text-anchor="middle">4s</text>
+//      <line x1="60" y1="25" x2="60" y2="55" stroke="grey" stroke-width=".5" />
+//      <text x="85" y="20" text-anchor="middle">6s</text>
+//      <line x1="85" y1="25" x2="85" y2="55" stroke="grey" stroke-width=".5" />
+//      <text x="110" y="20" text-anchor="middle">8s</text>
+//      <line x1="110" y1="25" x2="110" y2="55" stroke="grey" stroke-width=".5" />
+//
+//      <line x1="10" y1="30" x2="110" y2="30" stroke="grey" stroke-width=".5" />
+//      <line x1="10" y1="55" x2="110" y2="55" stroke="grey" stroke-width=".5" />
+//  </svg>
+
+//go:build js
+// +build js
+
+// fixme: bug. https://lists.w3.org/Archives/Public/www-svg/2006Jan/0490.html
+
+package main
+
+import (
+	"github.com/helmutkemper/iotmaker.webassembly/browser/factoryBrowser"
+	"github.com/helmutkemper/iotmaker.webassembly/browser/html"
+	"github.com/helmutkemper/iotmaker.webassembly/platform/factoryColor"
+	"time"
+)
+
+func main() {
+
+	done := make(chan struct{}, 0)
+
+	stage := factoryBrowser.NewStage()
+
+	s1 := factoryBrowser.NewTagSvg().
+		Width(120).
+		Height(120).
+		ViewBox([]float64{0, 0, 120, 120}).
+		XmlnsXLink("http://www.w3.org/1999/xlink").
+		Append(
+
+			// animated rectangles
+			factoryBrowser.NewTagSvgRect().X(10).Y(35).Height(15).Width(0).Append(
+				factoryBrowser.NewTagSvgAnimate().AttributeName("width").From(0).To(100).Begin("accessKey(s)").Dur(8*time.Second).Fill("freeze"),
+			),
+
+			// trigger
+			factoryBrowser.NewTagSvgText().X(60).Y(80).TextAnchor(html.KSvgTextAnchorMiddle).Style("pointer-events:none;").Text("Hit the \"s\" key"),
+
+			// grid
+			factoryBrowser.NewTagSvgText().X(10).Y(20).TextAnchor(html.KSvgTextAnchorMiddle).Text("0s"),
+			factoryBrowser.NewTagSvgLine().X1(10).Y1(25).X2(10).Y2(55).Stroke(factoryColor.NewGray()).StrokeWidth(0.5),
+			factoryBrowser.NewTagSvgText().X(35).Y(20).TextAnchor(html.KSvgTextAnchorMiddle).Text("2s"),
+			factoryBrowser.NewTagSvgLine().X1(35).Y1(25).X2(35).Y2(55).Stroke(factoryColor.NewGray()).StrokeWidth(0.5),
+			factoryBrowser.NewTagSvgText().X(60).Y(20).TextAnchor(html.KSvgTextAnchorMiddle).Text("4s"),
+			factoryBrowser.NewTagSvgLine().X1(60).Y1(25).X2(60).Y2(55).Stroke(factoryColor.NewGray()).StrokeWidth(0.5),
+			factoryBrowser.NewTagSvgText().X(85).Y(20).TextAnchor(html.KSvgTextAnchorMiddle).Text("6s"),
+			factoryBrowser.NewTagSvgLine().X1(85).Y1(25).X2(85).Y2(55).Stroke(factoryColor.NewGray()).StrokeWidth(0.5),
+			factoryBrowser.NewTagSvgText().X(110).Y(20).TextAnchor(html.KSvgTextAnchorMiddle).Text("8s"),
+			factoryBrowser.NewTagSvgLine().X1(110).Y1(25).X2(110).Y2(55).Stroke(factoryColor.NewGray()).StrokeWidth(0.5),
+
+			factoryBrowser.NewTagSvgLine().X1(10).Y1(30).X2(110).Y2(30).Stroke(factoryColor.NewGrey()).StrokeWidth(0.5),
+			factoryBrowser.NewTagSvgLine().X1(10).Y1(55).X2(110).Y2(55).Stroke(factoryColor.NewGrey()).StrokeWidth(0.5),
+		)
+
+	stage.Append(s1)
+
+	<-done
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//

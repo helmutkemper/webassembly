0bdcc38f27015f45bee92c00d5bf74e99bee22b1
Helmut Kemper <helmut.kemper@gmail.com>
2022-07-05 12:12:36 -0300

    working

--- examples/svg/Attributes/dy/Example1/main.go
+++ examples/svg/Attributes/dy/Example1/main.go
@@ -1,13 +1,13 @@
 // English:
 //
-// This example was taken from https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/dur
+// This example was taken from https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/dy
 //
 //   Notes:
 //     * The CSS is inside the example HTML file.
 //
 // Português:
 //
-// Este exemplo foi retirado do site https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/dur
+// Este exemplo foi retirado do site https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/dy
 //
 //   Notas:
 //     * O CSS está dentro do arquivo HTML de exemplo.
@@ -24,15 +24,15 @@
 //
 //  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
 //    <!-- Lines materialized the position of the glyphs -->
-//    <line x1="0"  x2="100%" y1="50%" y2="50%" />
-//    <line x1="10%" x2="10%" y1="0"   y2="100%" />
-//    <line x1="60%" x2="60%" y1="0"   y2="100%" />
+//    <line x1="10%" x2="10%"  y1="0"   y2="100%" />
+//    <line x1="0"   x2="100%" y1="30%" y2="30%"  />
+//    <line x1="0"   x2="100%" y1="80%" y2="80%"  />
 //
 //    <!-- Some reference text -->
-//    <text x="10%" y="50%" fill="grey">SVG</text>
+//    <text x="10%" y="30%" fill="grey">SVG</text>
 //
-//    <!-- The same text with a shift along the x-axis -->
-//    <text dx="50%" x="10%" y="50%">SVG</text>
+//    <!-- The same text with a shift along the y-axis -->
+//    <text dy="50%" x="10%" y="30%">SVG</text>
 //  </svg>
 
 //go:build js
@@ -53,15 +53,15 @@ func main() {
 
 	s1 := factoryBrowser.NewTagSvg().ViewBox([]float64{0, 0, 100, 100}).Append(
 		// Lines materialized the position of the glyphs
-		factoryBrowser.NewTagSvgLine().X1(float32(0.0)).X2(float32(1.0)).Y1(float32(0.5)).Y2(float32(0.5)),
 		factoryBrowser.NewTagSvgLine().X1(float32(0.1)).X2(float32(0.1)).Y1(float32(0.0)).Y2(float32(1.0)),
-		factoryBrowser.NewTagSvgLine().X1(float32(0.6)).X2(float32(0.6)).Y1(float32(0.0)).Y2(float32(1.0)),
+		factoryBrowser.NewTagSvgLine().X1(float32(0.0)).X2(float32(1.0)).Y1(float32(0.3)).Y2(float32(0.3)),
+		factoryBrowser.NewTagSvgLine().X1(float32(0.0)).X2(float32(1.0)).Y1(float32(0.8)).Y2(float32(0.8)),
 
 		// Some reference text
-		factoryBrowser.NewTagSvgText().X(float32(0.1)).Y(float32(0.5)).Fill(factoryColor.NewGray()).Text("SVG"),
+		factoryBrowser.NewTagSvgText().X(float32(0.1)).Y(float32(0.3)).Fill(factoryColor.NewGray()).Text("SVG"),
 
 		// The same text with a shift along the x-axis
-		factoryBrowser.NewTagSvgText().Dx(float32(0.5)).X(float32(0.1)).Y(float32(0.5)).Text("SVG"),
+		factoryBrowser.NewTagSvgText().Dy(float32(0.5)).X(float32(0.1)).Y(float32(0.3)).Text("SVG"),
 	)
 
 	stage.Append(s1)

ec67751d4dbc53d09d4a36cfbe1c54899514bfec
Helmut Kemper <helmut.kemper@gmail.com>
2022-07-09 23:14:59 -0300

    working

--- examples/svg/attributes/path/main.go
+++ examples/svg/attributes/path/main.go
@@ -11,7 +11,7 @@
 //          d="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50" />
 //
 //    <text>
-//      <textPath path="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50">
+//      <textPath href="#MyPath" path="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50">
 //        Quick brown fox jumps over the lazy dog.
 //      </textPath>
 //    </text>

262959ea2a9d6d3b756ad8f359c48e34ae1c0956
Helmut Kemper <helmut.kemper@gmail.com>
2022-08-08 23:41:36 -0300

    working

--- examples/battery/main.go
+++ examples/battery/main.go
@@ -11,25 +11,19 @@ import (
 func main() {
 
 	div := factoryBrowser.NewTagDiv()
-
 	stage := factoryBrowser.NewStage()
 	stage.Append(div)
 
 	var batteryEvent = make(chan battery.Data)
-
 	go func() {
 		for {
 			select {
 			case data := <-batteryEvent:
 				text := ""
-				text += fmt.Sprintf("event name: %v", data.EventName)
-				text += "<br>"
+				text += fmt.Sprintf("event name: %v<br>", data.EventName)
 				text += fmt.Sprintf("level: %v", data.Level)
-				text += "<br>"
 				text += fmt.Sprintf("charging: %v", data.Charging)
-				text += "<br>"
 				text += fmt.Sprintf("charging time: %v", data.ChargingTime)
-				text += "<br>"
 				text += fmt.Sprintf("discharging time: %v", data.DischargingTime)
 				div.Html(text)
 			}
